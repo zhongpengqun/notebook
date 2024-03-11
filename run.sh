6a828b1435ff1c516b5b97d2bee6c73aa1864ceb
zhong (钟鹏群) <3602551038@qq.com>
2024-03-11 14:30:55 +0800

    update

--- run.sh
+++
@@ -1,6 +0,0 @@
-python3 -m myvenv venv
-source myvenv/bin/activate
-
-pip3 install -r requirements-2.txt
-mkdocs serve -a 0.0.0.0:7779
-
--- sourceforge.md
+++
@@ -1,4 +0,0 @@
-sourceforge
-https://sourceforge.net/p/zhongpengqun2-github-io/code/ci/main/tree/
-z********n
-第一个字母大写***@*6
